3db8d88817ef05eb61ba74357e7b57b6745108c5
Tarcillo <tarcillo98gaziri@gmail.com>
2022-04-24 07:51:32 -0300

    first commit

---
+++ tutorial.f90
@@ -0,0 +1,10 @@
+program fortrantutorial
+    implicit none
+    character*20 :: name 
+
+    print *, "what's your name?"
+    read *, name
+    print *, "Hello ", name 
+
+
+end program fortrantutorial
